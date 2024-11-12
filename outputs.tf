6ab69e00bff77d67728e24d65308ea3dd66cf6fb
aikdp <kdprasad028@gmail.com>
2024-11-12 21:59:22 +0530

    terraform VPC module

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "vpc_id_mine" {
+    value = module.vpc.vpc_id
+}
+
+# output "az_zones"{
+#     value = module.vpc.az_info
+# }
---
+++ vpc.tf
@@ -0,0 +1,10 @@
+module "vpc"{
+    source = "../01-terraform-aws-vpc"
+    vpc_cidr = var.vpc_cidr_kdp
+    project_name = var.vpc_project_name
+    environment = var.vpc_environment
+    common_tags = var.common_tags_vpc
+    public_subnet_cidrs = var.subnet_public_cidrs
+    private_subnet_cidrs = var.subnet_private_cidrs
+    database_subnet_cidrs = var.subnet_database_cidrs
+}
